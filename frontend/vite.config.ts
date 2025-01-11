678a96c11d2b3350ac78ee3942ec4d2ae1c74c27
Lee Janzen-Morel <leejanzen91@gmail.com>
2025-01-10 21:11:52 -0800

    test: adds a plug in that may help

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -1,15 +1,13 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
+import vercel from "vite-plugin-vercel";
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [react(), vercel()],
   server: {
     proxy: {
       "/api": "http://localhost:5000",
     },
-  },
-  build: {
-    outDir: "build",
   },
 });
